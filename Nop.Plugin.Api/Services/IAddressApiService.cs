7a50902352b396f74ff1307654cbd3d3e4aa1fea
Mehmet <unal.m1991@gmail.com>
2021-08-17 14:53:39 +0300

    start

---
+++ Nop.Plugin.Api/Services/IAddressApiService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Plugin.Api.DTO;
+
+namespace Nop.Plugin.Api.Services
+{
+	public interface IAddressApiService
+	{
+		Task<IList<AddressDto>> GetAddressesByCustomerIdAsync(int customerId);
+		Task<AddressDto> GetCustomerAddressAsync(int customerId, int addressId);
+		Task<IList<CountryDto>> GetAllCountriesAsync(bool mustAllowBilling = false, bool mustAllowShipping = false);
+		Task<AddressDto> GetAddressByIdAsync(int addressId);
+	}
+}
